a23a7b6f679bf097bb98efd7c3c44b8cc1e68054
adanciules <adanciules@las.ch>
2015-09-14 17:23:08 +0200

    board drawing works

--- draw_board.js
+++ draw_board.js
@@ -10,24 +10,9 @@ var board = [];
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+var X_turn = true;
+
 init_board();
-
-var i = 0;
-var j = 0;
-for(i=0;i<num_squares;i++){
-    zeroes[i] = [];
-    exes[i] = [];
-    board[i] = [];
-    for(j = 0; j<num_squares;j++){
-        zeroes[i][j] = 0;
-        exes[i][j] = 0;
-        board[i][j] = "";
-    }
-}
-
-
-
-
 
 
 function draw_columns(){
@@ -50,13 +35,47 @@ function draw_rows(){
     }
 }
 
-function draw_pieces(){
+function init_pieces(){
+    var i = 0;
+    var j = 0;
+    for(i=0;i<num_squares;i++){
+        zeroes[i] = [];
+        exes[i] = [];
+        board[i] = [];
+        for(j = 0; j<num_squares;j++){
+            zeroes[i][j] = 0;
+            exes[i][j] = 0;
+            board[i][j] = "";
+        }
+    }
+}
 
+function draw_pieces(){
+    var i = 0;
+    var j = 0;
+    for(i=0;i<num_squares;i++){
+        for(j = 0; j<num_squares;j++){
+            if (board[i][j] == "X"){
+                draw_X(i,j);
+            }
+            else if(board[i][j] == "O"){
+                draw_O(i,j);
+            }
+        }
+    }
 }
 
 function draw_X(x,y){
 
-    
+    // draw \
+    ctx.moveTo(x*square_size + (square_size/4), y*square_size + (square_size/4));
+    ctx.lineTo(x*square_size + (3*square_size/4), y*square_size + (3*square_size/4));
+    ctx.stroke();
+
+    //draw /
+    ctx.moveTo(x*square_size + (square_size/4), y*square_size + (3*square_size/4));
+    ctx.lineTo(x*square_size + (3*square_size/4), y*square_size + (square_size/4));
+    ctx.stroke();
 }
 function draw_O(x,y){
     ctx.beginPath();
@@ -64,10 +83,38 @@ function draw_O(x,y){
     ctx.stroke();
 }
 
+function handle_click(event){
+    //random 7 offset
+    var x = Math.floor((event.pageX -10)/square_size);
+    var y = Math.floor((event.pageY-10)/square_size);
 
+    if(X_turn){
+        board[x][y] = "X";
+        exes[x][y] = 1;
+        X_turn = false;
+
+    }
+    else{
+        board[x][y] = "O";
+        zeroes[x][y] = 1;
+        X_turn = true;
+    }
+    draw_pieces();
+}
 
 function init_board() {
+    init_pieces();
     draw_columns();
     draw_rows();
-    draw_O(3,4);
+    draw_pieces();
+    c.addEventListener('click', handle_click, false);
+}
+
+function check_win(){
+
+}
+
+function tick(){
+    draw_pieces();
+    check_win();
 }
